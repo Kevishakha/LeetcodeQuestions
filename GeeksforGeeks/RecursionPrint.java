34390aab7826d0fc6d5c970d232ea92293652f00
vishakha <kevishakha@gmail.com>
2025-03-11 12:15:31 +0530

    Create RecursionPrint.java

---
+++ GeeksforGeeks/RecursionPrint.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public void printNos(int n) {
+        // Your code here
+        
+        
+        if(n==0){
+            return;
+        }
+        printNos(n-1);
+        System.out.print(n + " ");
+        
+        
+        
+    }
+}
