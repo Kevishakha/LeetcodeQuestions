acde05425585f3f0f65e038dd193776d5a99248e
vishakha <kevishakha@gmail.com>
2025-03-19 22:31:39 +0530

    Create NinjaSquaretri.java

---
+++ CodingNinjas/Contests/BeginnerContest82/NinjaSquaretri.java
@@ -0,0 +1,14 @@
+// Ninja has an array 'A' of N integers. He has to perform 'Q' operations with that array.
+// The operations are of two types:
+// • 0 X: add an integer X to the array.
+// • 1 X: remove integer X from the array (It is guaranteed that X exists in the array).
+// After each operation, using the numbers in the array as the lengths of the sides of a rectangle or a square, you have to tell him whether he can make at least one square and one rectangle.
+// There is no condition on the adjacent sides of the rectangle; they can be different or equal, but the opposite side needs to be equal.
+// Return an array of integers, where each element represents an operation. The element will be '1' if it is possible to make a rectangle and a square after that operation; otherwise, it will be '0'.
+// Example:
+// 'N' = 6
+// 'A' = [1, 1, 1, 2, 1, 1]
+// 'Q' = 3
+// 'B' = [[0, 2], [0, 1], [1, 1]]
+
+
