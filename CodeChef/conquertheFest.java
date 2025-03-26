27a450bb18f7c7c08a63be283f3a6499289d0a72
vishakha <kevishakha@gmail.com>
2025-03-26 20:48:20 +0530

    Create conquertheFest.java

---
+++ CodeChef/conquertheFest.java
@@ -0,0 +1,16 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int B=sc.nextInt();
+		if(N>=10*B){
+		    System.out.print("YES");
+		}else{
+		     System.out.print("NO");
+		}
+
+	}
+}
